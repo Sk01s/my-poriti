00f2150fc94cb7c5b7e089780e95920d9c6c429f
youssef sk <alsarakibiy@gmail.com>
2022-03-25 22:36:50 +0400

    add form and compres the photo

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ let aboutObsSection = new IntersectionObserver((el) => {
 aboutObsSection.observe(aboutSecetion);
 
 // onScrool animation
-const animationParentElem = document.querySelectorAll("[data-onscroll-par]");
+const animationParentElem = document.querySelector("[data-onscroll-par]");
 const animationElem = document.querySelectorAll("[data-onscroll]");
 const onscrollAnimationObs = new IntersectionObserver(
 	(e) => {
@@ -34,5 +34,49 @@ const onscrollAnimationObs = new IntersectionObserver(
 		threshold: 0.1,
 	}
 );
-onscrollAnimationObs.observe(animationParentElem[0]);
-onscrollAnimationObs.observe(animationParentElem[1]);
+onscrollAnimationObs.observe(animationParentElem);
+window.addEventListener("DOMContentLoaded", function () {
+	// get the form elements defined in your form HTML above
+
+	var form = document.querySelector("[data-form]");
+	var status = document.querySelector("[data-messege-form]");
+	console.log(status);
+
+	// Success and Error functions for after the form is submitted
+
+	function success() {
+		form.reset();
+		status.classList.add("success");
+		status.innerHTML = "Thanks!";
+	}
+
+	function error() {
+		status.classList.add("error");
+		status.innerHTML = "Oops! There was a problem.";
+	}
+
+	// handle the form submission event
+
+	form.addEventListener("submit", function (ev) {
+		ev.preventDefault();
+		var data = new FormData(form);
+		ajax(form.method, form.action, data, success, error);
+	});
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState !== XMLHttpRequest.DONE) return;
+		if (xhr.status === 200) {
+			success(xhr.response, xhr.responseType);
+		} else {
+			error(xhr.status, xhr.response, xhr.responseType);
+		}
+	};
+	xhr.send(data);
+}
